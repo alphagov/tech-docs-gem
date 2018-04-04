15cb3edf2325429771cc97ac2a8fa497d666157c
Tijmen Brommet <tijmen@gmail.com>
2018-04-04 13:28:14 +0100

    Raise a better error for pages without a H1 tag
    It often happens that people forget to add a h1 tag. The headings
    parsing code can't handle this and crashes. Helpfully, it will tell the
    user:
    
    ```
    undefined method `depth' for nil:NilClass
    ```
    
    This commit changes the behaviour to raise a more informative error,
    namely:
    
    ```
    No H1 tag found. You have to at least add one H1 heading to the page.
    ```
    
    Even better would be to avoid the error altogether, but I'm having a
    lot of trouble understanding the recursive nature of
    `HeadingTreeBuilder`.

--- lib/govuk_tech_docs/table_of_contents/helpers.rb
+++ lib/govuk_tech_docs/table_of_contents/helpers.rb
@@ -9,6 +9,11 @@ module GovukTechDocs
     module Helpers
       def table_of_contents(html, max_level: nil)
         headings = HeadingsBuilder.new(html).headings
+
+        if headings.none? { |heading| heading.size == 1 }
+          raise "No H1 tag found. You have to at least add one H1 heading to the page."
+        end
+
         tree = HeadingTreeBuilder.new(headings).tree
         HeadingTreeRenderer.new(tree, max_level: max_level).html
       end
