e10974c613947bc99fdfaabb5b1d3c144fab9ca3
lewisnyman <hello@lewisnyman.co.uk>
2018-07-02 17:23:52 +0100

    GTD-1: Add a test to ensure sentences are marked an split correctly. The function has to be public to unit test it. Also, display *five* sentences...

---
+++ spec/javascripts/search-spec.js
@@ -0,0 +1,22 @@
+describe('Search', function() {
+  'use strict';
+
+  var module;
+  var content;
+  var query;
+  var processedContent;
+  var expectedResults;
+
+  beforeEach(function() {
+    module = new GOVUK.Modules.Search();
+    processedContent = module.processContent(content, query);
+  });
+
+  it('returns concatenated sentences that contain the search query', function() {
+    expect(processedContent).toEqual(expectedResults)
+  });
+
+  query = 'test';
+  expectedResults = ' … This is <mark data-markjs="true">test</mark> sentence one … This is <mark data-markjs="true">test</mark> sentence two … This is <mark data-markjs="true">test</mark> sentence three … This is <mark data-markjs="true">test</mark> sentence four … This is <mark data-markjs="true">test</mark> sentence five … '
+  content = 'This sentence should not be shown. This is test sentence one. This is test sentence two? This is test sentence three:\nThis is test sentence four! This sentence should not be shown! This is test sentence five\n'
+});
