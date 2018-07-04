621922689552d02225fddb13520a7e56e74013a1
lewisnyman <hello@lewisnyman.co.uk>
2018-07-04 10:27:15 +0100

    GTD-1: Move var assignments close above the lines they are used

--- spec/javascripts/search-spec.js
+++ spec/javascripts/search-spec.js
@@ -9,14 +9,14 @@ describe('Search', function() {
 
   beforeEach(function() {
     module = new GOVUK.Modules.Search();
+    query = 'test';
+    content = 'This sentence should not be shown. This is test sentence one. This is test sentence two? This is test sentence three:\nThis is test sentence four! This sentence should not be shown! This is test sentence five\n'
     processedContent = module.processContent(content, query);
   });
 
   it('returns concatenated sentences that contain the search query', function() {
+    expectedResults = ' … This is <mark data-markjs="true">test</mark> sentence one … This is <mark data-markjs="true">test</mark> sentence two … This is <mark data-markjs="true">test</mark> sentence three … This is <mark data-markjs="true">test</mark> sentence four … This is <mark data-markjs="true">test</mark> sentence five … '
     expect(processedContent).toEqual(expectedResults)
   });
 
-  query = 'test';
-  expectedResults = ' … This is <mark data-markjs="true">test</mark> sentence one … This is <mark data-markjs="true">test</mark> sentence two … This is <mark data-markjs="true">test</mark> sentence three … This is <mark data-markjs="true">test</mark> sentence four … This is <mark data-markjs="true">test</mark> sentence five … '
-  content = 'This sentence should not be shown. This is test sentence one. This is test sentence two? This is test sentence three:\nThis is test sentence four! This sentence should not be shown! This is test sentence five\n'
 });
