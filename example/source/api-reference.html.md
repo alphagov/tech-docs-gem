2cacadcb8818f16486cfa6ca9301e0e35aacdca9
lewisnyman <hello@lewisnyman.co.uk>
2018-07-30 14:54:47 +0100

    GTD-30: Add integration tests for full api and single path output. Also for some reason whitespace in erb files is being output in the html. This is documented behaviour, but I'm not sure why this is happening now

---
+++ example/source/api-reference.html.md
@@ -0,0 +1,5 @@
+---
+title: Example API Petstore
+---
+
+api>
--- spec/features/integration_spec.rb
+++ spec/features/integration_spec.rb
@@ -26,6 +26,14 @@ RSpec.describe "The tech docs template" do
 
     when_i_view_child_of_expired_page
     then_the_page_highlighted_in_the_navigation_is("Expired page")
+
+    when_i_view_an_api_reference_page
+    then_there_is_correct_api_info_content
+    then_there_is_correct_api_path_content
+    then_there_is_correct_api_schema_content
+
+    when_i_view_an_single_path_api_reference_page
+    then_there_is_correct_api_path_content
   end
 
   def when_the_site_is_created
@@ -55,7 +63,7 @@ RSpec.describe "The tech docs template" do
   end
 
   def then_the_page_highlighted_in_the_navigation_is(link_label)
-    expect(page.find('li.active a').text).to eq(link_label)
+    page.find('#navigation li.active a', text: link_label)
   end
 
   def then_there_are_navigation_headings_from_other_pages
@@ -91,5 +99,36 @@ RSpec.describe "The tech docs template" do
 
   def when_i_view_child_of_expired_page
     visit '/child-of-expired-page.html'
+  end
+
+  def when_i_view_an_api_reference_page
+    visit '/api-reference.html'
+  end
+
+  def when_i_view_an_single_path_api_reference_page
+    visit '/api-path.html'
+  end
+
+  def then_there_is_correct_api_info_content
+    # Title
+    expect(page).to have_css('h1', :text => 'Swagger Petstore v1.0.0')
+    # Description
+    expect(page).to have_css('p', :text => 'A sample API that uses a petstore as an example to demonstrate features in the OpenAPI 3.0 specification')
+    # Base URL
+    expect(page).to have_css('strong', :text => 'http://petstore.swagger.io/v1')
+  end
+
+  def then_there_is_correct_api_path_content
+    # Path title
+    expect(page).to have_css('h2#get-pets', :text => 'GET /pets')
+    # Path parameters
+    expect(page).to have_css('table', :text => /\b(How many items to return at one time)\b/)
+    # Link to schema
+    expect(page).to have_css('table a[href="#schema-error"]')
+  end
+
+  def then_there_is_correct_api_schema_content
+    # Schema title
+    expect(page).to have_css('h3#schema-pet', :text => 'Pet')
   end
 end
