9e61b3b71fa60739fd46e9b56a571ee9b6da283a
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2026-05-28 13:56:19 +0100

    test: add unit tests for multi-content-type response rendering

--- spec/api_reference/renderer_spec.rb
+++ spec/api_reference/renderer_spec.rb
@@ -3,23 +3,25 @@ require "capybara/rspec"
 require "govuk_tech_docs/api_reference/api_reference_renderer"
 
 RSpec.describe GovukTechDocs::ApiReference::Renderer do
+  before(:each) do
+    @spec = {
+      "openapi": "3.0.0",
+      "info": {
+        "title": "title",
+        "version": "0.0.1",
+        "description": "# This is a header\n\nAnd a piece of _text_",
+      },
+      "paths": {},
+      "components": { "schemas": {} },
+    }
+    @app = double("Middleman::Application")
+    @config_context = double("Middleman::ConfigContext")
+    allow(@app).to receive(:config_context).and_return @config_context
+    allow(@config_context).to receive(:app).and_return @app
+    allow(@app).to receive(:api) { |arg| arg }
+  end
+
   describe ".api_full" do
-    before(:each) do
-      @spec = {
-        "openapi": "3.0.0",
-        "info": {
-          "title": "title",
-          "version": "0.0.1",
-          "description": "# This is a header\n\nAnd a piece of _text_",
-        },
-        "paths": {},
-      }
-      @app = double("Middleman::Application")
-      @config_context = double("Middleman::ConfigContext")
-      allow(@app).to receive(:config_context).and_return @config_context
-      allow(@config_context).to receive(:app).and_return @app
-      allow(@app).to receive(:api) { |arg| arg }
-    end
 
     it "renders the description" do
       document = Openapi3Parser.load(@spec)
@@ -77,6 +79,129 @@ RSpec.describe GovukTechDocs::ApiReference::Renderer do
       expect(rendered).to have_css("div#server-list>a", text: "https://dev.example.com")
       expect(rendered).to have_css("div#server-list>p>strong", text: "Development")
       expect(rendered).to have_css("div#server-list>p>strong>p>em", text: "Development")
+    end
+  end
+
+  describe "response content types" do
+    it "renders a response with no content" do
+      @spec[:paths] = {
+        "/things": {
+          "post": {
+            "summary": "Create a thing",
+            "responses": {
+              "201": { "description": "Created" },
+            },
+          },
+        },
+      }
+      document = Openapi3Parser.load(@spec)
+
+      render = described_class.new(@app, document)
+      expect { render.api_full(document.info, document.servers) }.not_to raise_error
+    end
+
+    it "renders a JSON response body from schema" do
+      @spec[:components] = { "schemas": { "Thing": { "properties": { "id": { "type": "integer" } } } } }
+      @spec[:paths] = {
+        "/things": {
+          "get": {
+            "summary": "Get things",
+            "responses": {
+              "200": {
+                "description": "OK",
+                "content": {
+                  "application/json": { "schema": { "$ref": "#/components/schemas/Thing" } },
+                },
+              },
+            },
+          },
+        },
+      }
+      document = Openapi3Parser.load(@spec)
+
+      render = described_class.new(@app, document)
+      rendered = Capybara::Node::Simple.new(render.api_full(document.info, document.servers))
+
+      expect(rendered).to have_css("em", text: "application/json")
+      expect(rendered).to have_css("pre code", text: /"id"/)
+    end
+
+    it "renders multiple content types" do
+      @spec[:components] = { "schemas": { "Thing": { "properties": { "id": { "type": "integer" } } } } }
+      @spec[:paths] = {
+        "/things": {
+          "get": {
+            "summary": "Get things",
+            "responses": {
+              "200": {
+                "description": "OK",
+                "content": {
+                  "application/json": { "schema": { "$ref": "#/components/schemas/Thing" } },
+                  "text/plain": { "schema": { "type": "string" }, "example": "hello" },
+                },
+              },
+            },
+          },
+        },
+      }
+      document = Openapi3Parser.load(@spec)
+
+      render = described_class.new(@app, document)
+      rendered = Capybara::Node::Simple.new(render.api_full(document.info, document.servers))
+
+      expect(rendered).to have_css("em", text: "application/json")
+      expect(rendered).to have_css("pre code", text: /"id"/)
+      expect(rendered).to have_css("em", text: "text/plain")
+      expect(rendered).to have_css("pre code", text: "hello")
+    end
+
+    it "renders non-JSON example as plain text" do
+      @spec[:paths] = {
+        "/things": {
+          "get": {
+            "summary": "Get things",
+            "responses": {
+              "200": {
+                "description": "OK",
+                "content": {
+                  "text/plain": { "schema": { "type": "string" }, "example": "plain text response" },
+                },
+              },
+            },
+          },
+        },
+      }
+      document = Openapi3Parser.load(@spec)
+
+      render = described_class.new(@app, document)
+      rendered = Capybara::Node::Simple.new(render.api_full(document.info, document.servers))
+
+      expect(rendered).to have_css("em", text: "text/plain")
+      expect(rendered).to have_css("pre code", text: "plain text response")
+    end
+
+    it "does not render non-JSON content without an example" do
+      @spec[:paths] = {
+        "/things": {
+          "get": {
+            "summary": "Get things",
+            "responses": {
+              "200": {
+                "description": "OK",
+                "content": {
+                  "application/octet-stream": { "schema": { "type": "string", "format": "binary" } },
+                },
+              },
+            },
+          },
+        },
+      }
+      document = Openapi3Parser.load(@spec)
+
+      render = described_class.new(@app, document)
+      rendered = Capybara::Node::Simple.new(render.api_full(document.info, document.servers))
+
+      expect(rendered).not_to have_css("pre code")
     end
   end
 end
