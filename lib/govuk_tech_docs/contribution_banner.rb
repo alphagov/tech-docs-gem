0e46080313192767ef35efedf74400f484f9d2f9
Connor Gurney <me@connorgurney.me.uk>
2026-09-15 20:34:26 +0100

    Change default branch from `master` to `main` in contribution banner
    A minor fix, perhaps, but it avoids a hop in the HTTPS journey from GOV.UK to GitHub. Given that it's breaking, I'll leave you fine folk to work out when this is suitable to go out.

--- lib/govuk_tech_docs/contribution_banner.rb
+++ lib/govuk_tech_docs/contribution_banner.rb
@@ -39,7 +39,7 @@ module GovukTechDocs
     end
 
     def repo_branch
-      config[:tech_docs][:github_branch] || "master" # TODO: change this to 'main' in a future breaking release
+      config[:tech_docs][:github_branch] || "main"
     end
 
   private
