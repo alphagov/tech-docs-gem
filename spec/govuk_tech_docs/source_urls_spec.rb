b4966382163c6adddb9ef025d60662ce16697946
A Haque <40199643+zaid3@users.noreply.github.com>
2026-09-15 19:55:55 +0100

    Test report issue URL configuration fallback

--- spec/govuk_tech_docs/source_urls_spec.rb
+++ spec/govuk_tech_docs/source_urls_spec.rb
@@ -10,7 +10,18 @@ RSpec.describe GovukTechDocs::SourceUrls do
       expect(source_urls.report_issue_url).to eql("https://github.com/test/repo/issues/new?body=Problem+with+%27title%27+%28https%3A%2F%2Ftest-docs%2Furl%29&labels=bug&title=Re%3A+%27title%27")
     end
 
-    it "uses a custom URL when configured" do
+    it "uses a custom URL configured in tech-docs.yml" do
+      current_page = generate_current_page("title", "url")
+      config = generate_config("test/repo", "https://test-docs/")
+      config[:tech_docs][:source_urls] = {
+        report_issue_url: "mailto:test@example.com",
+      }
+      source_urls = GovukTechDocs::SourceUrls.new(current_page, config)
+
+      expect(source_urls.report_issue_url).to eql("mailto:test@example.com?body=Problem+with+%27title%27+%28https%3A%2F%2Ftest-docs%2Furl%29&subject=Re%3A+%27title%27")
+    end
+
+    it "supports the legacy config.rb location" do
       current_page = generate_current_page("title", "url")
       config = generate_config("test/repo", "https://test-docs/")
       config[:source_urls] = {
